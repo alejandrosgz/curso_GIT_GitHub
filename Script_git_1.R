92912916ebe57ddaf59621c595056fc6e70a8930
Alejandro Sanchez Gomez <alejandro.sanchezgomez97@gmail.com>
2022-11-21 19:04:28 +0100

    add acript curso

---
+++ Script_git_1.R
@@ -0,0 +1,15 @@
+# install.packages("usethis")
+# install.packages("gitcreds")
+
+# library(usethis)
+# library(gitcreds)
+
+
+# use_git_config(user.name = "Jane Doe", user.email = "jane@example.org") permite configurar el nombre y usuario de git
+# para hacer la configuración inicial desde rstudio en vez de desde git shell.
+
+# create_github_token() # generar un token identificativo para el usuario de git-github
+
+# gitcreds_set() # acceder al Git credential store --> introducir el token generado
+
+
